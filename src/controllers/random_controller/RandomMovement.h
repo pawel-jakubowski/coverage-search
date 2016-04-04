bcda83105882007a1ef8c52929736054d761d3f3
Pawel Jakubowski <pawel-jakubowski@hotmail.com>
2016-04-05 00:05:04 +0200

    Modification to build system

---
+++ src/controllers/random_controller/RandomMovement.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <core/control_interface/ci_controller.h>
+#include <core/utility/math/vector2.h>
+#include <plugins/robots/generic/control_interface/ci_differential_steering_actuator.h>
+#include <plugins/robots/foot-bot/control_interface/ci_footbot_proximity_sensor.h>
+
+#include <random>
+
+namespace argos {
+
+class RandomMovement : public CCI_Controller {
+public:
+    RandomMovement();
+    virtual ~RandomMovement() {}
+
+    virtual void Init(TConfigurationNode& configuration);
+    virtual void ControlStep();
+    virtual void Reset() {}
+    virtual void Destroy() {}
+private:
+    CCI_DifferentialSteeringActuator* wheelsEngine;
+    CCI_FootBotProximitySensor* proximitySensor;
+    Real velocity;
+    Real minDistanceFromObstacle;
+    CRange<CDegrees> minAngleFromObstacle;
+
+    enum class Direction {
+        left, right
+    };
+
+    CVector2 getWeightedProximityReading();
+    bool isRoadClear(const CVector2& obstacleProximity);
+    Direction getRotationDirection(const CDegrees& obstacleAngle);
+    void rotate(Direction rotationDirection);
+};
+
+}
