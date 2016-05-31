d9e66916aef333f93105ff2a2139188a0ba656be
Pawel Jakubowski <pawel-jakubowski@hotmail.com>
2016-05-31 19:25:39 +0200

    Change getopt to argparse

--- experiments.py
+++ experiments.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import os
 import sys
-import getopt
+import argparse
 import errno
 import subprocess
 import json
@@ -80,6 +80,44 @@ def runExperiment(cmake, config):
     print "\t done!"
 
 
+def generateConfig(buildDir, experiment, robots, targets):
+    return {
+            "experiment" : experiment,
+            "robots" : robots,
+            "targets" : targets,
+            "log" : {
+                "path" : buildDir + "/results/" + experiment,
+                "name" : str(robots) + "_" + str(targets) + ".json"
+                }
+            }
+
+
+def getLogData(logConfig):
+    file = logConfig["path"] + "/" + logConfig["name"]
+    with open(file, mode='r') as jsonFile:
+        jsonData = json.load(jsonFile)
+    return jsonData
+
+
+def joinPlot(dataLabel, x, y):
+    baseline, = plt.plot(x, y, label=dataLabel)
+    plt.plot(x, y, 'o', color=baseline.get_color())
+
+
+def configureSubplot(ax, title, labels, integerAxis = ["default", "default"], xlim = None):
+    if integerAxis[0] is "int":
+        ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+    if integerAxis[1] is "int":
+        ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+    if xlim is not None:
+        plt.xlim(xlim)
+    plt.xlabel(labels[0])
+    plt.ylabel(labels[1])
+    plt.grid(True)
+    plt.legend(loc="best")
+    plt.title(title)
+
+
 def plotFoundTargets(buildDir, config, figNumber = 1):
     for experiment in config["experiments"]:
         fig = plt.figure(figNumber)
@@ -89,24 +127,8 @@ def plotFoundTargets(buildDir, config, figNumber = 1):
         for targets in config["targets"]:
             plt.subplot(subplotNumber)
             for robots in config["robots"]:
-                configuration = {
-                        "experiment" : experiment,
-                        "robots" : robots,
-                        "targets" : targets,
-                        "log" : {
-                            "path" : buildDir + "/results/" + experiment,
-                            "name" : str(robots) + "_" + str(targets) + ".json"
-                            }
-                        }
-                subplotFoundTargets(ax, configuration)
-            ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-            ax.yaxis.set_major_locator(MaxNLocator(integer=True))
-            plt.xlim([0, targets])
-            plt.ylabel("Step")
-            plt.xlabel("Found targets")
-            plt.grid(True)
-            plt.legend(loc="best")
-            plt.title(str(targets) + " targets")
+                subplotFoundTargets(ax, generateConfig(buildDir, experiment, robots, targets))
+                configureSubplot(ax, str(targets)+" targets", ["Found targets", "Step"], ["int", "int"], [0, targets])
             plt.tight_layout()
             subplotNumber += 1
         figNumber += 1
@@ -114,23 +136,19 @@ def plotFoundTargets(buildDir, config, figNumber = 1):
 
 
 def subplotFoundTargets(ax, config):
-    file = config["log"]["path"] + "/" + config["log"]["name"]
-    with open(file, mode='r') as json_file:
-        json_data = json.load(json_file)
+    data = getLogData(config["log"])
 
-    timeArray = []
-    targetsArray = []
+    timeArray = [0]
+    targetsArray = [0]
     foundTargets = 0
 
-    for target in json_data["targets"]:
+    for target in data["targets"]:
         foundTargets += 1
         timeArray.append(target["step"])
         targetsArray.append(foundTargets)
     
     timeArray.sort()
-    plotLabel = str(config["robots"]) + " robots"
-    baseline, = plt.plot(targetsArray, timeArray, label=plotLabel)
-    plt.plot(targetsArray, timeArray, 'o', color=baseline.get_color())
+    joinPlot(str(config["robots"]) + " robots", targetsArray, timeArray)
 
 
 def plotCoverage(buildDir, config, figNumber = 1):
@@ -142,22 +160,8 @@ def plotCoverage(buildDir, config, figNumber = 1):
         for targets in config["targets"]:
             plt.subplot(subplotNumber)
             for robots in config["robots"]:
-                configuration = {
-                        "experiment" : experiment,
-                        "robots" : robots,
-                        "targets" : targets,
-                        "log" : {
-                            "path" : buildDir + "/results/" + experiment,
-                            "name" : str(robots) + "_" + str(targets) + ".json"
-                            }
-                        }
-                subplotCoverage(ax, configuration)
-            ax.yaxis.set_major_locator(MaxNLocator(integer=True))
-            plt.ylabel("Step")
-            plt.xlabel("Coverage [%]")
-            plt.grid(True)
-            plt.legend(loc="best")
-            plt.title(str(targets) + " targets")
+                subplotCoverage(ax, generateConfig(buildDir, experiment, robots, targets))
+                configureSubplot(ax, str(targets)+" targets", ["Coverage [%]", "Step"], ["default", "int"])
             plt.tight_layout()
             subplotNumber += 1
         figNumber += 1
@@ -165,41 +169,81 @@ def plotCoverage(buildDir, config, figNumber = 1):
 
 
 def subplotCoverage(ax, config):
-    file = config["log"]["path"] + "/" + config["log"]["name"]
-    with open(file, mode='r') as json_file:
-        json_data = json.load(json_file)
+    data = getLogData(config["log"])
 
     timeArray = []
     coverageArray = []
 
-    for threshold in json_data["thresholds"]:
+    for threshold in data["thresholds"]:
         timeArray.append(threshold["step"])
         coverageArray.append(threshold["coverage"])
     
-    plotLabel = str(config["robots"]) + " robots"
-    baseline, = plt.plot(coverageArray, timeArray, label=plotLabel)
-    plt.plot(coverageArray, timeArray, 'o', color=baseline.get_color())
+    joinPlot(str(config["robots"]) + " robots", coverageArray, timeArray)
+
+
+def plotCompareCoverage(buildDir, config, figNumber = 1):
+    fig = plt.figure(figNumber)
+    ax = fig.gca()
+    subplotNumber = len(config["robots"])*100 + 11
+    for robots in config["robots"]:
+        plt.subplot(subplotNumber)
+        for experiment in config["experiments"]:
+            coverageArray = []
+            timeArray = []
+            for targets in config["targets"]:
+                configuration = generateConfig(buildDir, experiment, robots, targets)
+                index = 0
+                for threshold in getLogData(configuration["log"])["thresholds"]:
+                    try:
+                        timeArray[index] = threshold["step"]
+                        coverageArray[index] = threshold["coverage"]
+                    except IndexError:
+                        timeArray.append(threshold["step"])
+                        coverageArray.append(threshold["coverage"])
+                    index += 1
+            numberOfSamples = len(timeArray)
+            timeArray = [x / numberOfSamples for x in timeArray]
+            joinPlot(experiment, coverageArray, timeArray)
+            configureSubplot(ax, str(robots)+" robots", ["Coverage [%]", "Step"], ["default", "int"])
+        plt.tight_layout()
+        subplotNumber += 1
+    return figNumber
+
+
+def plotCompareTargets(buildDir, config, figNumber = 1):
+    fig = plt.figure(figNumber)
+    ax = fig.gca()
+    subplotNumber = len(config["robots"])*100 + 11
+    for robots in config["robots"]:
+        plt.subplot(subplotNumber)
+        for experiment in config["experiments"]:
+            targetsArray = [0]
+            timeArray = [0]
+            targets = config["targets"][-1]
+            configuration = generateConfig(buildDir, experiment, robots, targets)
+            targetsNumber = 1
+            for target in getLogData(configuration["log"])["targets"]:
+                timeArray.append(target["step"])
+                targetsArray.append(targetsNumber)
+                targetsNumber += 1
+            timeArray.sort()
+            joinPlot(experiment, targetsArray, timeArray)
+            configureSubplot(ax, str(robots)+" robots", ["Targets", "Step"], ["int", "int"], [0, targets])
+        plt.tight_layout()
+        subplotNumber += 1
+    return figNumber
+
 
 
 #======#
 # MAIN #
 #======#
-def main(argv):
-    try:
-        opts, args = getopt.getopt(argv,"ntc",["no-exe","targets","coverage"])
-    except getopt.GetoptError:
-        print sys.argv[0] + ' -n -t -c'
-        sys.exit(2)
-    execute = True
-    targets = False
-    coverage = False
-    for opt, arg in opts:
-        if opt in ("-n", "--no-exe"):
-            execute = False
-        elif opt in ("-t", "--targets"):
-            targets = True
-        elif opt in ("-c", "--coverage"):
-            coverage = True
+def main():
+    parser = argparse.ArgumentParser(description="Execute ARGoS experiments")
+    plotTypes = ["targets", "coverage", "compare-coverage", "compare-targets"]
+    parser.add_argument("--no-exe", action="store_true", help="Do not execute experiments")
+    parser.add_argument("--plot", nargs="+", choices=plotTypes, default=[], type=str, help="Plot given plot-type")
+    args = parser.parse_args()
 
     sourceDir = os.getcwd()
     buildDir = sourceDir + "/build/release"
@@ -212,13 +256,17 @@ def main(argv):
             }
 
     figNumber = 1
-    if execute:
+    if not args.no_exe:
         runExperiments(cmake, configuration)
-    if targets:
+    if "targets" in args.plot:
         figNumber = plotFoundTargets(buildDir, configuration, figNumber)
-    if coverage:
+    if "coverage" in args.plot:
         figNumber = plotCoverage(buildDir, configuration, figNumber)
+    if "compare-coverage" in args.plot:
+        figNumber = plotCompareCoverage(buildDir, configuration, figNumber)
+    if "compare-targets" in args.plot:
+        figNumber = plotCompareTargets(buildDir, configuration, figNumber)
     plt.show()
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
